9cb7a3d9e763b51b8ca0216df312f3d1d8d3b6f9
Zesshan110 <zh1101214@gmail.com>
2024-02-24 07:06:03 +0500

    incmplete work

---
+++ app.js
@@ -0,0 +1,65 @@
+// // Answer no1
+// var name = +prompt("Enter your value")
+// if(value / 6){
+//     true
+// }else if( value/6){
+//     false
+// }else( console.log("invalid Number"))
+
+
+// // ==============================================
+
+// // Answer no2
+// var name = +prompt("Enter your name")
+// var english = +prompt("english")
+// var urdu = +prompt("urdu")
+// var computer = +prompt("computer")
+// var science = +prompt("science")
+// var SST = +prompt("SST")
+// var math = +prompt("math")
+// var total = english +urdu + computer + science + SST 
+// + mathvar
+// var num = +prompt(800)
+// if(num >=700 && num <=800){
+//  console.log( name +" your grade is A+" );
+// } else if( num>=500 && num <=699){
+//     console.log( name +" your grade is A" + num );
+//    }else if(num >=400 && num <=499){
+//     console.log( name +" your grade is B " + num );
+//    }else if(num >=300 && num <=299){
+//     console.log( name +" your grade is c" + num );
+//    }else if(num >=100 && num <=199){
+//     console.log( name +" your grade is fail" + num );
+//    }else( console.log("invlid Number"))
+
+
+// // =================================================
+
+
+
+
+
+// // Answer no3
+// let name = +prompt("Enter your name")
+// let english = +prompt("english")
+// let urdu = +prompt("urdu")
+// let computer = +prompt("computer")
+// let science = +prompt("science")
+// let SST = +prompt("SST")
+// let math = +prompt("math")
+// let total = english +urdu + computer + science + SST 
+// + math
+// let percentage = (total/600)*100;
+// if(percentage >=91 && percentage <=100){
+//  console.log( name +" your grade is A+" + percentage );
+// } else if(percentage >=60 && percentage <=90){
+//     console.log( name +" your grade is A" + percentage );
+//    }else if(percentage >=50 && percentage <=59){
+//     console.log( name +" your grade is B " + percentage );
+//    }else if(percentage >=35 && percentage <=49){
+//     console.log( name +" your grade is c" + percentage );
+//    }else if(percentage >=10 && percentage <=25){
+//     console.log( name +" your grade is fail" + percentage );
+//    }else( console.log("invlid Number"))
+
+// // ======================================================
